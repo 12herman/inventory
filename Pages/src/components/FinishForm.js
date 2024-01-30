5e9c8699b8438d28264910ea5cfacf3efd1e8646
Herman <108892289+12herman@users.noreply.github.com>
2024-01-30 14:34:26 +0530

    Revert "Hamthaanbranch"

---
+++ Pages/src/components/FinishForm.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Form} from 'antd'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faPen, faInfoCircle, faLocationDot, faLandmark, faCircleCheck, faPeopleGroup, faL } from "@fortawesome/free-solid-svg-icons"
+const {form}= Form;
+
+export default function FinishForm() {
+  return (
+    <section >
+      <div className='mt-5 flex justify-center items-center'><FontAwesomeIcon className='text-2xl ' icon={faPeopleGroup} /></div>
+
+      <Form form={form} className='h-[60.9vh] mt-5'>
+
+        
+      </Form>
+
+    </section>
+  )
+}
--- Pages/src/redux/store.js
+++ Pages/src/redux/store.js
@@ -1,25 +1,20 @@
 import {configureStore} from '@reduxjs/toolkit';
 import officeSlice from './slices/officeSlice';
 import employeeSlice from './slices/employeeSlice';
-import roleDetailSlice from './slices/roleDetailSlice';
+import roleSlice from './slices/roleSlice';
 import accountdetailsSlice from './slices/accountdetailsSlice';
-import accessoriesSlice from './slices/accessoriesSlice';
-import brandSlice from './slices/brandSlice';
-import consoleSlice from './slices/consoleSlice';
-import productSlice from './slices/productSlice';
+import departmentSlice from './slices/departmentSlice';
+import roleDetailsSlice from './slices/roleDetailsSlice';
 
 export const store = configureStore({
     devTools:true,
     reducer:{
         office: officeSlice,
         employee: employeeSlice,
-        roledetails: roleDetailSlice,
-        account:accountdetailsSlice,      
-        accessories:accessoriesSlice,
-        brand:brandSlice,
-        consoles:consoleSlice,
-        products:productSlice
-        
+        role: roleSlice,
+        account:accountdetailsSlice,
+        department:departmentSlice,
+        roledetail:roleDetailsSlice
     }
 });
 
