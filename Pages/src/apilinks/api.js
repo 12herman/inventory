c8557f5e1e0ebc55849947b332e7fc67c2a5ee82
Herman <108892289+12herman@users.noreply.github.com>
2024-01-30 14:16:12 +0530

    Revert "Revert "Hamthaanbranch""

--- Pages/src/apilinks/api.js
+++ Pages/src/apilinks/api.js
@@ -1,10 +1,12 @@
 export const ApiLink = {
     office: "https://localhost:7129/api/OfficeLocation",
+    //officePut:"https://localhost:7129/api/OfficeLocation/id:int?id=49",
     employedetails:"https://localhost:7129/api/EmployeeDetails",
-    role:"https://localhost:7129/api/Role",
+    roledetails:"https://localhost:7129/api/Role",
     account:"https://localhost:7129/api/AccountDetails",
-    department:"https://localhost:7129/api/Department",
-    roledetail:"https://localhost:7129/api/RoleDetail",
-    leaderemployee:"https://localhost:7129/api/LeaderAndEmployee",
+    products:"https://localhost:7129/api/Productsquantity",
+    accessories:"https://localhost:7129/api/Accessory",
+    brand:"https://localhost:7129/api/Brand",
+    consoles:"https://localhost:7129/api/Console"
   };
   
