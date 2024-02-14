6566ca87bc2acaf11030b9cc78ee80d9cb98fba8
12herman <josephherman810@gmail.com>
2024-02-14 09:45:00 +0530

    employeeleadermergeupdate

--- Pages/src/apilinks/api.js
+++ Pages/src/apilinks/api.js
@@ -5,7 +5,7 @@ export const ApiLink = {
     account:"https://localhost:7129/api/AccountDetails",
     department:"https://localhost:7129/api/Department",
     roledetail:"https://localhost:7129/api/RoleDetail",
-    leaderemployee:"https://localhost:7129/api/LeaderAndEmployee",
+    // leaderemployee:"https://localhost:7129/api/LeaderAndEmployee",
     accessories:"https://localhost:7129/api/Accessory",
     brand:"https://localhost:7129/api/Brand",
     consoles:"https://localhost:7129/api/Console",
